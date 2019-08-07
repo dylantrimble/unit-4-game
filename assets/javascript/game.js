729300d53a94808509f7b70c760a3216268ad964
dyl trimble <dyltrimble12@gmail.com>
2019-08-07 18:10:01 -0400

    for the most part working javaScript with a few issues with resets and making the program run smoothly. having issues with crystal number setting, other than that working code

---
+++ assets/javascript/game.js
@@ -0,0 +1,81 @@
+$("document").ready(function(){
+
+    console.log("ready to go!")
+
+
+
+var targetNumber = 0;
+var crystalValue = "";
+var numberOfWins = 0;
+var numberOfLosses = 0;
+var score = 0;
+
+function startGame() {
+
+    score = 0;
+
+    $("#score").text(score);
+  
+   targetNumber = Math.floor((Math.random() * 100) + 20 );
+    
+   $("#randomNumber").text(targetNumber);
+
+    for(var i = 0; i < 1; i++){
+
+        var value = Math.floor((Math.random() * 12) + 1);
+
+        crystalValue = value + crystalValue;
+    }
+
+    console.log(crystalValue)
+}
+startGame()
+
+
+
+$("#cage").on("click", function(){
+    
+    crystalValue = parseInt(crystalValue);
+
+    score += crystalValue
+    
+    $("#score").text(score);
+
+    if (score === targetNumber) {
+
+        $("#status").text("You Win");
+
+        numberOfWins++;
+
+        $("#wins").text("wins: " + numberOfWins);
+
+        startGame();
+
+      }
+  
+      else if (score >= targetNumber) {
+        $("#status").text("You Loose!")
+
+        numberOfLosses++;
+
+        $("#losses").text("Losses: " + numberOfLosses);
+
+        startGame();
+      }
+      
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+});
